5a9e8895f5e05cf544109b842ecb3b8d2dfafbd4
vitordellani <vitordellani@gmail.com>
2025-10-27 14:53:07 -0300

    chore: add frontend assets and backend configuration files
    - Add favicon and icon assets for frontend
    - Add sound files for frontend notifications
    - Add .eslintignore and prettier.config.js for backend linting
    - Add .editorconfig for consistent code formatting
    - Remove old whaticket database and configuration files

--- whaticket/backend/dist/helpers/SendRefreshToken.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SendRefreshToken = void 0;
-exports.SendRefreshToken = (res, token) => {
-    res.cookie("jrt", token, { httpOnly: true });
-};
